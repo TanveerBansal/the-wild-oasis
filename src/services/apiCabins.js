cc307f671affcda1c6cffb67a8afa49ea94f6bf3
TanveerBansal <singhtanveer020@gmail.com>
2024-11-21 11:32:17 +0530

    add cabin menus

--- src/services/apiCabins.js
+++ src/services/apiCabins.js
@@ -45,7 +45,7 @@ export async function createEditCabin(newCabin, id) {
   }
 
   // 2.upload Image
-  if(hasImagePath) return data
+  if (hasImagePath) return data
   const { error: storageError } = await supabase
     .storage
     .from('cabin-images')
--- src/ui/Modal.jsx
+++ src/ui/Modal.jsx
@@ -1,9 +1,4 @@
-import {
-  cloneElement,
-  createContext,
-  useContext,
-  useState,
-} from "react";
+import { cloneElement, createContext, useContext, useState } from "react";
 import { createPortal } from "react-dom";
 import { HiXMark } from "react-icons/hi2";
 import styled from "styled-components";
@@ -79,7 +74,7 @@ function Open({ children, opens: opensWindowName }) {
 
 function Window({ children, name }) {
   const { openName, close } = useContext(ModalContext);
-  const{ref} = useOutsideClick(close)
+  const { ref } = useOutsideClick(close);
   // const ref = useRef();
 
   // useEffect(() => {
